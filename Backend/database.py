2f3b718cbaac91833e562a7dbf398f122cbe8ee5
elkettani <youssef.elkettani@stud.th-luebeck.de>
2025-09-04 00:21:57 +0200

    FIX: Database URL mit Umgebungsvariablen

--- Backend/database.py
+++ Backend/database.py
@@ -1,10 +1,19 @@
+from urllib.parse import quote_plus
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
 
-DATABASE_URL = f"postgresql+psycopg2://postgres:{POSTGRES_PASSWORD}@localhost:5432/postgres"
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
+
+
+encoded_password = quote_plus(POSTGRES_PASSWORD)
+
+
+DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{encoded_password}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -17,4 +26,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
