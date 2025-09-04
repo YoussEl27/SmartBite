fc33c7ab87e04276aff0c4fa6d8f4980b43855bb
elkettani <youssef.elkettani@stud.th-luebeck.de>
2025-09-04 20:39:37 +0200

    Fix: update

--- Backend/database.py
+++ Backend/database.py
@@ -3,13 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
+POSTGRES_PASSWORD = quote(os.getenv("POSTGRES_PASSWORD", ""))
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
-
-
 
 DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
 
