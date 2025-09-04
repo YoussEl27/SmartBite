e9431d2a59988d0df37fe284490ce0b0b3f7186d
elkettani <youssef.elkettani@stud.th-luebeck.de>
2025-09-04 20:18:45 +0200

    Fix: update

--- Backend/database.py
+++ Backend/database.py
@@ -10,10 +10,8 @@ POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
 
 
-encoded_password = quote(POSTGRES_PASSWORD)
 
-
-DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{encoded_password}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
+DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
