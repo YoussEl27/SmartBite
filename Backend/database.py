16800fa4f36dc812d5117297b5c4cbea3888f5d1
YoussEl27 <elkrttaniysf15@gmail.com>
2026-05-28 01:09:03 +0200

    Fix: Nutze DATABASE_URL falls vorhanden

--- Backend/database.py
+++ Backend/database.py
@@ -3,17 +3,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = quote(os.getenv("POSTGRES_PASSWORD", ""))
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
+if not DATABASE_URL:
+    POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD = quote(os.getenv("POSTGRES_PASSWORD", ""))
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
+    POSTGRES_DB = os.getenv("POSTGRES_DB", "postgres")
+
+    DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
